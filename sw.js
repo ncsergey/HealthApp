8e7cad891c9fbec67b508c6f87659f8aae0f37ba
ncsergey <ncsergey@gmail.com>
2026-08-28 21:12:28 +0300

    new

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "health-app-static-v126"; // Версия также отображается в шапке приложения.
+const CACHE_NAME = "health-app-static-v127"; // Версия также отображается в шапке приложения.
 const CACHE_PREFIX = "health-app-static-";
 const APP_SHELL = [
   "./",
